e4c78fe85cafe47296af94469ee536fac96fb50b
rozzlessa <rodrigo@tgld.co>
2014-06-20 13:23:45 -0400

    update database credentials

--- app/config/development/database.php
+++ app/config/development/database.php
@@ -7,7 +7,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'FootballManager',
+			'database'  => 'footballManager',
 			'username'  => 'root',
 			'password'  => 'roadmin',
 			'charset'   => 'utf8',
